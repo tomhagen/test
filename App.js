38b771b6454de370d9934fa4a51b7cfac6438d27
tomhagen <tom@mrs.com.vn>
2020-09-01 18:04:50 +0700

    add error

--- App.js
+++ App.js
@@ -23,7 +23,7 @@ import {
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
-import Icon from 'react-native-vector-icons/AntDesign';
+
 
 const App = () => {
   return (
